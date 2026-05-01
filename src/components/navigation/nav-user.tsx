3d4bd297342077a6542af8eb0e59753a31782de1
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2026-05-01 18:52:37 +1000

    Refactor NavUser component to streamline dropdown menu options
    - Removed 'Upgrade to Pro', 'Billing', and 'Notifications' items from the dropdown menu for a cleaner user experience.
    - Retained essential options such as 'Account' to maintain functionality while simplifying the interface.

--- src/components/navigation/nav-user.tsx
+++ src/components/navigation/nav-user.tsx
@@ -121,23 +121,8 @@ export function NavUser({
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                <SparklesIcon />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
                 <BadgeCheckIcon />
                 Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCardIcon />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <BellIcon />
-                Notifications
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
